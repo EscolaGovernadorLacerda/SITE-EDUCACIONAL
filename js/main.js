cbd82c98e770bd0424745075e8b4de78f4ec46ff
EscolaGovernadorLacerda <130990739+EscolaGovernadorLacerda@users.noreply.github.com>
2023-07-19 22:23:02 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+function normal() {
+  var root = document.querySelector(":root");
+  root.style.setProperty("--cor-fundo", "#BABBBF");
+  root.style.setProperty("--cor-fonte", "black");
+  root.style.setProperty("--cor-fonte-menus", "#fff");
+  root.style.setProperty("--cor-fonte-drop", "#333");
+  root.style.setProperty("--cor-fundo-drop", "#fff");
+  root.style.setProperty("--cor-fundo-menus", "#083a6f");
+  root.style.setProperty("--cor-bordas-menus", "white");
+  root.style.setProperty("--cor-ao-passar", "#0085BA");
+  root.style.setProperty("--cor-borda-inferior", "lightslategray");
+}
+
+function preto() {
+  var root = document.querySelector(":root");
+  root.style.setProperty("--cor-fundo", "black");
+  root.style.setProperty("--cor-fonte", "white");
+  root.style.setProperty("--cor-fonte-menus", "black");
+  root.style.setProperty("--cor-fonte-drop", "white");
+  root.style.setProperty("--cor-fundo-drop", "black");
+  root.style.setProperty("--cor-fundo-menus", "white");
+  root.style.setProperty("--cor-bordas-menus", "black");
+  root.style.setProperty("--cor-ao-passar", "#333");
+  root.style.setProperty("--cor-borda-inferior", "white");
+}
+
+function branco() {
+  var root = document.querySelector(":root");
+  root.style.setProperty("--cor-fundo", "white");
+  root.style.setProperty("--cor-fonte", "black");
+  root.style.setProperty("--cor-fonte-menus", "white");
+  root.style.setProperty("--cor-fonte-drop", "black");
+  root.style.setProperty("--cor-fundo-drop", "white");
+  root.style.setProperty("--cor-fundo-menus", "black");
+  root.style.setProperty("--cor-bordas-menus", "white");
+  root.style.setProperty("--cor-ao-passar", "#BABBBF");
+  root.style.setProperty("--cor-borda-inferior", "black");
+}
